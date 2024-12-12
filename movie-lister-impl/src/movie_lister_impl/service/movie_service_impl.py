f6511657a07a7b24dd78d8437a9a80d050e7df93
Vincent Reynard Satyadharma <vincent_reynard@hotmail.com>
2024-12-12 23:54:31 +1100

    Fix MovieService get_movies_by_director return type

--- movie-lister-impl/src/movie_lister_impl/service/movie_service_impl.py
+++ movie-lister-impl/src/movie_lister_impl/service/movie_service_impl.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from dependency_injector.wiring import Provide, inject
 from movie_lister_api.provider.movie_provider import MovieProvider
 from movie_lister_api.service.model.movie import Movie
@@ -12,5 +14,5 @@ class MovieServiceImpl:
     ):
         self.movie_provider = movie_provider
 
-    def get_movies_by_director(self, director: str) -> Movie:
+    def get_movies_by_director(self, director: str) -> List[Movie]:
         return self.movie_provider.get_movies_by_director(director=director)
