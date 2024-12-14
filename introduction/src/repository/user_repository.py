6dc2ca2d7c6dfde0d9447d2b5f765a3bd60715ca
Vincent Reynard Satyadharma <vincent_reynard@hotmail.com>
2024-12-14 15:58:47 +1100

    Remove introduction project

--- introduction/src/repository/user_repository.py
+++
@@ -1,9 +0,0 @@
-from typing import Dict
-
-
-class UserRepository:
-    def __init__(self) -> None:
-        pass
-
-    def get_user_by_id(self, id: int) -> Dict[str, str]:
-        return {'id': id, 'name': 'Vincent'}
